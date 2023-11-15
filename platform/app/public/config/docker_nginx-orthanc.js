cf4a07ca6e2e9b59eef848d9e5fd5b525c3763c0
tingmon2 <tingmon2@gmail.com>
2023-11-15 14:59:45 +0900

    first commit

---
+++ platform/app/public/config/docker_nginx-orthanc.js
@@ -0,0 +1,47 @@
+window.config = {
+  routerBasename: '/',
+  showStudyList: true,
+  extensions: [],
+  modes: [],
+  // below flag is for performance reasons, but it might not work for all servers
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  strictZSpacingForVolumeViewport: true,
+  defaultDataSourceName: 'dicomweb',
+  dataSources: [
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'dicomweb',
+      configuration: {
+        friendlyName: 'Orthanc Server',
+        name: 'Orthanc',
+        // wadoUriRoot: 'http://34.64.212.199:8080/dicom-web',
+        // qidoRoot: 'http://34.64.212.199:8080/dicom-web',
+        // wadoRoot: 'http://34.64.212.199:8080/dicom-web',
+        wadoUriRoot: '/dicom-web',
+        qidoRoot: '/dicom-web',
+        wadoRoot: '/dicom-web',
+        qidoSupportsIncludeField: false,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        omitQuotationForMultipartRequest: true,
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomjson',
+      sourceName: 'dicomjson',
+      configuration: {
+        friendlyName: 'dicom json',
+        name: 'json',
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
+      sourceName: 'dicomlocal',
+      configuration: {
+        friendlyName: 'dicom local',
+      },
+    },
+  ],
+};
